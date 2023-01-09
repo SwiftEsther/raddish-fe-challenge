bbe96c864e29b0c2404de5b1cd056c39c12364b8
Esther Akinloose <estherakinloose@gmail.com>
2023-01-09 09:58:54 +0000

    Add connect/disconnect wallet feature with mainnet as the initial chain Tested on localhost

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    ALCHEMY_ID: 'W2nXipo-rIudJ1OqJ7xXGUtSw1OiTaR3',
+  }
 }
 
 module.exports = nextConfig
